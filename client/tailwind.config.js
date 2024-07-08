c0900742f75f77f38b3842f28421a91f6820122a
Sofialv05 <sofalvsy@gmail.com>
2024-07-08 21:05:01 +0700

    feat: add router in client side

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        first: "#80BCBD",
+        second: "#AAD9BB",
+        third: "#D5F0C1",
+        fourth: "#D5F0C1",
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ server/models/cat.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cat.init({
+    name: DataTypes.STRING,
+    breed: DataTypes.STRING,
+    age: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    adoptionStatus: DataTypes.STRING,
+    description: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Cat',
+  });
+  return Cat;
+};
