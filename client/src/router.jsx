23fe50fb1e1ae041df9483bcbea98216da3e9eb9
Sofialv05 <sofalvsy@gmail.com>
2024-07-08 21:29:31 +0700

    style: add tailwind and flowbite config colors

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,3 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
-export const router = createBrowserRouter([{ path: "/register", element: "" }]);
+export const router = createBrowserRouter([
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/", element: <Home /> },
+]);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,23 @@
+import flowbite from "flowbite-react/tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        first: "#80BCBD",
-        second: "#AAD9BB",
-        third: "#D5F0C1",
-        fourth: "#D5F0C1",
+        light: {
+          first: "#EEEDEB",
+          second: "#CCD3CA",
+          third: "#B5C0D0",
+        },
+        dark: {
+          first: "#4B5563",
+          second: "#879E8D",
+          third: "#B5C0D0",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
