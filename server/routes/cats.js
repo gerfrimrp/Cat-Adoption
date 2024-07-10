06d20f06738413622a975273373c3b36abc91146
Sofialv05 <sofalvsy@gmail.com>
2024-07-10 09:11:56 +0700

    fix: routes on server side

--- server/routes/cats.js
+++ server/routes/cats.js
@@ -1,10 +1,8 @@
 const CatController = require("../controllers/CatController");
-const authentication = require("../middlewares/authentication");
 const { authorizeCat } = require("../middlewares/authorization");
 
 const router = require("express").Router();
 
-router.use(authentication);
 router.get("/", CatController.findUserCats);
 router.post("/", CatController.createCat);
 router.put("/:id", authorizeCat, CatController.editCat);
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,8 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
-const authentication = require("../middlewares/authentication");
+
 const { authorizeProfile } = require("../middlewares/authorization");
 
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
-router.post("/login/google", UserController.loginGoogle);
-
-router.use(authentication);
 router.post("/user-profile", UserController.createUserProfile);
 router.put(
   "/user-profile/edit",
