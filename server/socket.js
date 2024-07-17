e1cd82f9a5cc684248ce4ffe32a70c3ea9e27b1b
gerfrimrp <alans.gefri@gmail.com>
2024-07-18 04:01:52 +0700

    chore: chat

--- server/socket.js
+++ server/socket.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
 
 const messages = [
   {
-    message: "hello mba yolanda",
+    message: "hello",
     user: "system",
     createdAt: new Date(),
   },
@@ -23,41 +23,58 @@ const messages = [
 
 // io.use(authSocket);
 
+// io.on("connection", (socket) => {
+//   const UserId = socket.id;
+//   console.log(`${UserId} connected`);
+
+//   // userSocketMap.set(userId, socket.id);
+
+//   // socket.on("ping", (message) => {
+//   //   console.log({ message }, "dari client");
+//   // });
+
+//   socket.on("join-chat", (AuthorId) => {
+//     socket.join("test");
+//     console.log("joined a chat " + AuthorId);
+//   });
+//   socket.on("leave-chat", (chat) => {
+//     socket.leave(chat);
+//     console.log("leave chat " + chat);
+//   });
+
+//   socket.on("message:create", async ({ message, chat }) => {
+//     console.log({ message });
+//     io.to(chat).emit("message: delivered", { message });
+//     // try {
+//     //   await ChatMessage.create({
+//     //     message,
+//     //     chat,
+//     //   });
+//     // } catch (err) {
+//     //   console.error(err);
+//     // }
+//   });
+
+//   socket.on("disconnect", () => {
+//     // console.log(disconnected);
+//     // userSocketMap.delete(UserId);
+//   });
+// });
+
 io.on("connection", (socket) => {
-  const UserId = socket.id;
-  console.log(`${UserId} connected`);
-
-  // userSocketMap.set(userId, socket.id);
-
-  // socket.on("ping", (message) => {
-  //   console.log({ message }, "dari client");
-  // });
-
-  socket.on("join-chat", (AuthorId) => {
-    socket.join("test");
-    console.log("joined a chat " + AuthorId);
-  });
-  socket.on("leave-chat", (chat) => {
-    socket.leave(chat);
-    console.log("leave chat " + chat);
+  socket.on("join-chat", ({ AuthorId }) => {
+    socket.join("room 1");
+    console.log(`${AuthorId} joined room 1`);
   });
 
-  socket.on("message:create", async ({ message, chat }) => {
-    console.log({ message });
-    io.to(chat).emit("message: delivered", { message });
-    // try {
-    //   await ChatMessage.create({
-    //     message,
-    //     chat,
-    //   });
-    // } catch (err) {
-    //   console.error(err);
-    // }
+  socket.on("message:create", ({ message, chat }) => {
+    console.log("Received message:", message);
+    io.to(chat).emit("message:delivered", message);
+    // Save message to database or perform other operations if needed
   });
 
   socket.on("disconnect", () => {
-    console.log(` disconnected`);
-    // userSocketMap.delete(UserId);
+    // Handle disconnection if needed
   });
 });
 
